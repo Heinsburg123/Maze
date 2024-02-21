4296a1958aeed42e3c7845fdb961831baed0280e
Heinsburg123 <heinsburg2005@gmail.com>
2024-02-21 22:16:31 +0700

    order code and add new buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from functools import cmp_to_key
 from collections import deque 
 import math
@@ -14,6 +15,7 @@ class vec:
     def __init__(self,a,b):
         self.x=a.x-b.x
         self.y=a.y-b.y 
+
 class segment:
     def __init__(self,x1,y1,x2,y2):
         self.x1,self.y1,self.x2,self.y2,self.a,self.b=x1,y1,x2,y2,y1-y2,x2-x1
@@ -24,6 +26,20 @@ class segment:
         self.c=self.a*x1+self.b*y1
         self.length=(x1-x2)**2+(y1-y2)**2
 
+class node:
+    def __init__(self, a_star,path, cur):
+        self.a_star=a_star
+        self.path=path
+        self.cur=cur
+    def __lt__(self,other):
+        return self.a_star<other.a_star
+
+node_fr=[(0,0),(1,0),(1,4),(2,4),(2,0),(5,0),(5,1),(3,1),(3,2),(5,2),(5,3),(3,3),(3,4),(6,4),(6,0),(9,0),(9,1),(7,1),(7,2),(9,2),(9,3),(7,3),(7,4),(10,4),(10,6)]
+node_back=[(0,0),(-1,0),(-1,-1),(-10,-1),(-10,-6)]
+vertice=[]
+ans={}
+dem=0
+
 def cross(a,b):
     return (a.x*b.y-a.y*b.x)
 
@@ -33,19 +49,22 @@ def equal(a,b):
     else:
         return False
 
-def check_inter(line1,line2):
+def check_inter(line1,line2,Click):
     det=line1.a*line2.b-line2.a*line1.b
-    if(det==0):
-        return True
-    X=(line2.b*line1.c - line1.b*line2.c)/det
-    Y=(line1.a*line2.c-line2.a*line1.c)/det
     p1=point(line1.x1,line1.y1)
     p2=point(line1.x2,line1.y2)
     p3=point(line2.x1,line2.y1)
     p4=point(line2.x2,line2.y2)
     if(equal(p1,p3) or equal(p1,p4) or equal(p2,p3) or equal(p2,p4)):
         return False
-    if(min(line1.x1,line1.x2)<=X<=max(line1.x1,line1.x2) and min(line1.y1,line1.y2)<=Y<=max(line1.y1,line1.y2)) and (min(line2.x1,line2.x2)<=X<=max(line2.x1,line2.x2) and min(line1.y1,line1.y2)<=Y<=max(line2.y1,line2.y2)):
+    if(det==0):
+        return False
+    X=(line2.b*line1.c - line1.b*line2.c)/det
+    Y=(line1.a*line2.c-line2.a*line1.c)/det
+
+    if(min(line1.x1,line1.x2)<=X<=max(line1.x1,line1.x2) and min(line1.y1,line1.y2)<=Y<=max(line1.y1,line1.y2) and min(line2.x1,line2.x2)<=X<=max(line2.x1,line2.x2) and min(line2.y1,line2.y2)<=Y<=max(line2.y1,line2.y2) and Click==False):
+        return True
+    elif(min(line1.x1,line1.x2)<X<max(line1.x1,line1.x2) and min(line1.y1,line1.y2)<=Y<=max(line1.y1,line1.y2) and min(line2.x1,line2.x2)<=X<=max(line2.x1,line2.x2) and min(line2.y1,line2.y2)<Y<max(line2.y1,line2.y2) and Click==True):
         return True
     else:
         return False
@@ -74,59 +93,47 @@ def dis_to_p(p,seg1):
     else:
         seg2.y2=(seg2.c-seg2.x2*seg2.a)/seg2.b
     p_new=point(X,Y)
-    if(check_inter(seg1,seg2)):
+    if(check_inter(seg1,seg2,False)):
         return dis(p,p_new)
     else:
         return min(dis(p,point(seg1.x1,seg1.y1)),dis(p,point(seg1.x2,seg1.y2)))
 
-c=Canvas(root,bg='white',height=700,width=1500)
-
-node_fr=[(0,0),(1,0),(1,4),(2,4),(2,0),(5,0),(5,1),(3,1),(3,2),(5,2),(5,3),(3,3),(3,4),(6,4),(6,0),(9,0),(9,1),(7,1),(7,2),(9,2),(9,3),(7,3),(7,4),(10,4),(10,6)]
-node_back=[(0,0),(-1,0),(-1,-1),(-10,-1),(-10,-6)]
-vertice=[]
-edge=[]
-c.place(x=0,y=80)
-
-def draw(x,y,k):
-    vertice.append(point(x,y))
-    for i in range(1,25):
-        c.create_line(x+29*node_fr[i-1][0],y+29*node_fr[i-1][1],x+29*node_fr[i][0],y+29*node_fr[i][1])
-        vertice.append(point(x+29*node_fr[i][0],y+29*node_fr[i][1]))
-    x_end,y_end=x+29*node_fr[24][0],y+29*node_fr[24][1]
+def draw(x,y,k,size,add):
+    if(add):
+        vertice.append(point(x,y))
+    for i in range(1,len(node_fr)):
+        c.create_line(x+size*node_fr[i-1][0],y+size*node_fr[i-1][1],x+size*node_fr[i][0],y+size*node_fr[i][1])
+        if(add):   
+            vertice.append(point(x+size*node_fr[i][0],y+size*node_fr[i][1]))
+    x_end,y_end=x+size*node_fr[len(node_fr)-1][0],y+size*node_fr[len(node_fr)-1][1]
     for i in range(k-1):
-        for j in range(2,25):
-            c.create_line(x_end+29*node_fr[j-1][0]-29,y_end+29*node_fr[j-1][1],x_end+29*node_fr[j][0]-29,y_end+29*node_fr[j][1])
-            vertice.append(point(x_end+29*node_fr[j][0]-29,y_end+29*node_fr[j][1]))
-        x_end,y_end=x_end+29*node_fr[24][0]-29,y_end+29*node_fr[24][1]
-    for i in range(1,5):
-        c.create_line(x_end+29*node_back[i-1][0],y_end+29*node_back[i-1][1],x_end+29*node_back[i][0],y_end+29*node_back[i][1])
-        vertice.append(point(x_end+29*node_back[i][0],y_end+29*node_back[i][1]))
-    x_end,y_end=x_end+29*node_back[4][0],y_end+29*node_back[4][1]
+        for j in range(2,len(node_fr)):
+            c.create_line(x_end+size*node_fr[j-1][0]-size,y_end+size*node_fr[j-1][1],x_end+size*node_fr[j][0]-size,y_end+size*node_fr[j][1])
+            if(add):
+                vertice.append(point(x_end+size*node_fr[j][0]-size,y_end+size*node_fr[j][1]))
+        x_end,y_end=x_end+size*node_fr[len(node_fr)-1][0]-size,y_end+size*node_fr[len(node_fr)-1][1]
+    for i in range(1,len(node_back)):
+        c.create_line(x_end+size*node_back[i-1][0],y_end+size*node_back[i-1][1],x_end+size*node_back[i][0],y_end+size*node_back[i][1])
+        if(add):
+            vertice.append(point(x_end+size*node_back[i][0],y_end+size*node_back[i][1]))
+    x_end,y_end=x_end+size*node_back[len(node_back)-1][0],y_end+size*node_back[len(node_back)-1][1]
     for i in range(k-1):
-        for j in range(2,5):
-            c.create_line(x_end+29*node_back[j-1][0]+29,y_end+29*node_back[j-1][1],x_end+29*node_back[j][0]+29,y_end+29*node_back[j][1])
-            vertice.append(point(x_end+29*node_back[j][0]+29,y_end+29*node_back[j][1]))
-        x_end,y_end=x_end+29*node_back[4][0]+29,y_end+29*node_back[4][1]
-    vertice.pop()
-draw(100,100,3)
-b1=Button(root, text='find')
-b2=Button(root,text='clear')
-b1.place(x=20,y=20)
-b2.place(x=100,y=20)
-
+        for j in range(2,len(node_back)):
+            c.create_line(x_end+size*node_back[j-1][0]+size,y_end+size*node_back[j-1][1],x_end+size*node_back[j][0]+size,y_end+size*node_back[j][1])
+            if(add):
+                vertice.append(point(x_end+size*node_back[j][0]+size,y_end+size*node_back[j][1]))
+        x_end,y_end=x_end+size*node_back[len(node_back)-1][0]+size,y_end+size*node_back[len(node_back)-1][1]
+    if(add):
+        vertice.pop()
 
 def check(heap,code,line):
     if(len(heap)==0):
         return False
     minn=heapq.nsmallest(1,heap)
     for line2 in code[minn[0]]:
-        if(check_inter(line,code[minn[0]][line2])==True):
+        if(check_inter(line,code[minn[0]][line2],False)==True):
             return True
     return False
-
-for i in range(len(vertice)):
-    vertice[i].y=-vertice[i].y
-    vertice[i].id=i
 
 def check_taunt(s1,s2,prev,pos,convex):
     if(convex==1):
@@ -134,8 +141,6 @@ def check_taunt(s1,s2,prev,pos,convex):
     if(cross(vec(s2,pos),vec(s1,s2))>0 and cross(vec(s2,prev),vec(s1,s2))<0):
         return False
     return True
-
-ans={}
 
 def line_of_sight(cen,extra):
     def cmp(a,b):
@@ -316,8 +321,6 @@ def line_of_sight(cen,extra):
                 code[dis2][f'{u},{v}']=seg2
                 heapq.heappush(heap,dis2)
 
-a=[[] for _ in range(1000)]
-
 def create_data():
     for i in range(len(vertice)):
         for j in range(i+1,len(vertice)):
@@ -344,64 +347,28 @@ def add_node(p,ind,size):
             a[size].append(i)
             a[i].append(size)
 
-dem=0
-p1=point(0,0)
-p2=point(0,0)
-
-for i in range(len(vertice)):
-    line_of_sight(vertice[i],0)
-
-create_data()
-def click(e):
-    global dem,p1,p2
-    if(dem>=2):
+def clear_node():
+    global dem
+    if(dem<2):
         return
-    dem+=1
-    if(dem==1):
-        p1=point(e.x,-e.y)
-        p1.id=-1
-        vertice.append(p1)
-        line_of_sight(p1,1)
-        add_node(p1,-1,len(vertice)-1)
-    else:
-        p2=point(e.x,-e.y)
-        p2.id=-2
-        vertice.append(p2)
-        line_of_sight(p2,2)
-        add_node(p2,-2,len(vertice)-1)
-        for i in range(len(vertice)):
-            for j in a[i]:
-                c.create_line(vertice[i].x,-vertice[i].y,vertice[j].x,-vertice[j].y,fill='red')
-        find_path()
-def find_path():
-    d=[100000000 for _ in range(1000)]
-    close=[0 for _ in range(1000)]
-    d[len(vertice)-2]=0
-    cha=[0 for _ in range(1000)]
-    code={}
-    heap=[]
-    code[0]=deque([len(vertice)-2])
-    heapq.heapify(heap)
-    heapq.heappush(heap,0)
-    while len(heap)>0:
-        dd=heapq.heappop(heap)
-        u=code[dd][0]
-        code[dd].popleft()
-        if(dd>d[u]):
-            continue
-        if(close[u]==1):
-            continue
-        close[u]=1
-        for v in a[u]:
-            if(d[u]+dis(vertice[u],vertice[v])>d[len(vertice)-1]):
-                continue
-            if(d[v]>d[u]+dis(vertice[u],vertice[v])):
-                d[v]=d[u]+dis(vertice[u],vertice[v])
-                cha[v]=u
-                heapq.heappush(heap,d[v])
-                if(code.get(d[v])==None):
-                    code[d[v]]=deque([])
-                code[d[v]].append(v)
+    for _ in range(2):
+        for i in range(len(vertice)-1):
+            if(ans[vertice[len(vertice)-1].id].get(vertice[i].id)!=None and ans[vertice[len(vertice)-1].id][vertice[i].id]==1):
+                a[i].pop()
+        a.pop()
+        vertice.pop()
+    ans.pop(-1)
+    ans.pop(-2)
+    c.delete('all')
+    draw(144,140,1,29,0)
+    dem=0
+
+def show_visi():
+    for i in range(len(vertice)):
+        for j in a[i]:
+            c.create_line(vertice[i].x,-vertice[i].y,vertice[j].x,-vertice[j].y,fill='red')
+
+def show_path():
     v=len(vertice)-1
     u=len(vertice)-2
     while(cha[v]!=u):
@@ -409,8 +376,96 @@ def find_path():
         v=cha[v]
     c.create_line(vertice[v].x,-vertice[v].y,vertice[cha[v]].x,-vertice[cha[v]].y,fill='blue')
 
+def raycast(p,extra):
+    check=0
+    ray=segment(p.x,p.y,100000,p.y)
+    for i in range(1,len(vertice)-extra):
+        seg=segment(vertice[(i-1)%(len(vertice)-extra)].x,vertice[(i-1)%(len(vertice)-extra)].y,vertice[i].x,vertice[i].y)
+        if(check_inter(ray,seg,True)):
+            check+=1
+    if(check%2!=0):
+        return True
+    else:
+        return False
 
+def click(e):
+    global dem,p1,p2
+    if(dem>=2):
+        return
+    dem+=1
+    if(dem==1):
+        if(raycast(point(e.x,-e.y),1)==False):
+            dem-=1
+            return 
+        p1=point(e.x,-e.y)
+        p1.id=-1
+        vertice.append(p1)
+        a.append([])
+        line_of_sight(p1,1)
+        add_node(p1,-1,len(vertice)-1)
+    else:
+        if(raycast(point(e.x,-e.y),2)==False):
+            dem-=1
+            return 
+        p2=point(e.x,-e.y)
+        p2.id=-2
+        vertice.append(p2)
+        a.append([])
+        line_of_sight(p2,2)
+        add_node(p2,-2,len(vertice)-1)
+        find_path()
 
+def find_path():
+    d=[100000000 for _ in range(len(vertice))]
+    d[len(vertice)-2]=0
+    close=[0 for _ in range(len(vertice))]
+    for i in range(len(vertice)):
+        cha[i]=0
+    heap=[]
+    heapq.heapify(heap)
+    heapq.heappush(heap,node(0,0,len(vertice)-2))
+    while len(heap)>0:
+        cur_node=heapq.heappop(heap)
+        u=cur_node.cur
+        if(cur_node.path>d[u]):
+            continue
+        if(close[u]==1):
+            continue
+        close[u]=1
+        for v in a[u]:
+            if(d[u]+dis(vertice[u],vertice[v])+dis(vertice[v],vertice[len(vertice)-1])>d[len(vertice)-1]):
+                continue
+            if(d[v]>d[u]+dis(vertice[u],vertice[v])):
+                d[v]=d[u]+dis(vertice[u],vertice[v])
+                cha[v]=u
+                heapq.heappush(heap,node(d[v]+dis(vertice[v],vertice[len(vertice)-1]),d[v],v))
+
+c=Canvas(root,bg='white',height=700,width=1500)
+c.place(x=0,y=80)
+draw(144,140,1,29,1)
+
+a=[[] for _ in range(len(vertice))]
+cha=[0 for _ in range(len(vertice)+2)]
+
+p1=point(0,0)
+p2=point(0,0)
+b1=ttk.Button(root, text='show graph',command=show_visi)
+b2=ttk.Button(root,text='clear',command=clear_node)
+b3=ttk.Button(root, text='show path', command=show_path)
+b1.place(x=20,y=20)
+b2.place(x=100,y=20)
+b3.place(x=180,y=20)
+
+for i in range(len(vertice)):
+    vertice[i].y=-vertice[i].y
+    vertice[i].id=i
+
+for i in range(len(vertice)):
+    cc=cross(vec(vertice[i],vertice[(i-1)%len(vertice)]),vec(vertice[(i+1)%len(vertice)],vertice[i]))
+    if(cc>0):
+        line_of_sight(vertice[i],0)
+
+create_data()
 
 c.bind('<Button 1>',click)
 c.pack()
